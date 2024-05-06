badb6102466e2e549952d362c8e53c6603861495
Vadim <test@e.com>
2024-05-06 08:33:44 +0200

    fix heading element

--- src/components/Actions/Actions.tsx
+++ src/components/Actions/Actions.tsx
@@ -1,24 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { TType } from '../../types/modules'
 import styles from './actions.module.scss'
-import {
-	Bold,
-	Eraser,
-	Italic,
-	Underline,
-	Quote,
-	Heading
-} from 'lucide-react'
+import {Bold,	Eraser,	Italic,	Underline, Quote, Heading } from 'lucide-react'
 
 type Props = {
 	applyFormat: (type: TType) => void
 	setText: (type: string) => void
 }
 
-const headArr = ['h1', 'h2', 'h3'];
+const headArr = [
+	{format: 'h1', title: 'h'},
+	{format: 'h2', title: 'h'},
+	{format: 'h3', title: 'h'},
+];
 
 const Actions = ({ applyFormat, setText }: Props) => {
 	// const[colorInput, setColorInput] = useState('#fff');
+	const refHeading = useRef<HTMLLIElement>(null);
 	const [open, setOpen] = useState(false)
 	// const colorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 	// setColorInput(e.target.value);
@@ -28,6 +26,20 @@ const Actions = ({ applyFormat, setText }: Props) => {
 		applyFormat(item)
 		setOpen(false)
 	}
+
+
+	useEffect(() => {
+    const clickSortHandler = (event: MouseEvent) => {
+      const e = event as MouseEvent;
+      if (refHeading.current && !e.composedPath().includes(refHeading.current)) {
+        setOpen(false);
+      }
+    };
+    document.body.addEventListener("click", clickSortHandler);
+    return () => {
+      document.body.removeEventListener("click", clickSortHandler);
+    };
+  }, []);
 
 	return (
 		<ul className={styles.actions}>
@@ -63,7 +75,7 @@ const Actions = ({ applyFormat, setText }: Props) => {
 				</button>
 			</li>
 
-			<li className={`${styles.item} ${styles.itemHeading}`}>
+			<li className={`${styles.item} ${styles.itemHeading}`} ref={refHeading}>
 				<button className={styles.btn} onClick={() => setOpen(!open)}>
 					<Heading />
 				</button>
@@ -71,11 +83,11 @@ const Actions = ({ applyFormat, setText }: Props) => {
 					className={styles.listHeading}
 					style={{ maxHeight: open ? '500px' : '0px' }}
 				>
-					{headArr.map(item => {
+					{headArr.map((item, index) => {
 						return(
-						<li key={item}>
-							<button className={styles.btn} onClick={() => changeTitle(item)}>
-								{item}
+						<li key={item.format}>
+							<button className={`${styles.btn} ${styles.btnHead}`} onClick={() => changeTitle(item.format)}>
+								{item.title} <span>{index + 1}</span>
 							</button>
 						</li>
 						)
